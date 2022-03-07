17afd7493e9174c98752c19cd305013d4e4639d1
Marvin-Magmodules <marvin@magmodules.eu>
2022-03-07 14:00:12 +0100

    Make Logger component compatible with Monolog 2.0

--- Logger/PaazlLogger.php
+++ Logger/PaazlLogger.php
@@ -1,20 +1,43 @@
 <?php
 /**
- * Copyright © 2019 Paazl. All rights reserved.
+ * Copyright © Paazl. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
 
 namespace Paazl\CheckoutWidget\Logger;
 
+use Magento\Framework\Serialize\Serializer\Json;
 use Monolog\Logger;
 
 /**
- * Class PaazlLogger
- *
- * @package Paazl\CheckoutWidget\Logger
+ * DebugLogger logger class
  */
 class PaazlLogger extends Logger
 {
+
+    /**
+     * @var Json
+     */
+    private $json;
+
+    /**
+     * DebugLogger constructor.
+     *
+     * @param Json   $json
+     * @param string $name
+     * @param array  $handlers
+     * @param array  $processors
+     */
+    public function __construct(
+        Json $json,
+        string $name,
+        array $handlers = [],
+        array $processors = []
+    ) {
+        $this->json = $json;
+        parent::__construct($name, $handlers, $processors);
+    }
 
     /**
      * @param string $type
@@ -22,12 +45,10 @@ class PaazlLogger extends Logger
      */
     public function add($type, $data)
     {
-        if (is_array($data)) {
-            $this->addInfo($type . ': ' . json_encode($data));
-        } elseif (is_object($data)) {
-            $this->addInfo($type . ': ' . json_encode($data));
+        if (is_array($data) || is_object($data)) {
+            $this->addRecord(static::INFO, $type . ': ' . $this->json->serialize($data));
         } else {
-            $this->addInfo($type . ': ' . $data);
+            $this->addRecord(static::INFO, $type . ': ' . $data);
         }
     }
 }
